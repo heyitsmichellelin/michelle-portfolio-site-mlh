31e56a25dbbfdffc97894a2454847663b3b4498f
“Michelle <itsmichellelin@gmail.com>
2023-07-13 23:34:44 -0700

    add timeline page

--- app/__init__.py
+++ app/__init__.py
@@ -81,6 +81,10 @@ def get_hobbies_page():
 def get_travels_page():
     return render_template('travels.html', title="Travels", url=os.getenv("URL"))
 
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline")
+
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
     name = request.form['name']
