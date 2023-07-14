6d9d0149d8a93a46ad270e8e8019d12371727876
“Michelle <itsmichellelin@gmail.com>
2023-07-13 21:22:09 -0700

    removed unnecessary quotations

--- curl-test.sh
+++ curl-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 before=$(curl --request GET http://localhost:5000/api/timeline_post) | jq length
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null -X POST http://localhost:5000/api/timeline_post -d 'name="Maxx"&email="maxx@gmail.com"&content=it is summertime'
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null -X POST http://localhost:5000/api/timeline_post -d 'name=Maxx&email=maxx@gmail.com&content=it is summertime'
 )
 if [[ "$status_code" -eq 200 ]] ; then
     after=$(curl --request GET http://localhost:5000/api/timeline_post | jq length)
