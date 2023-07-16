7dfcc1f4c604a454e9bec770f5704e3d94b8217c
“Michelle <itsmichellelin@gmail.com>
2023-07-15 20:58:37 -0700

    deploy using systemctl

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd portfolio-site-mlh/
 
 git fetch && git reset origin/main --hard
-
-git pull
 
 source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new -d -s portfolio
-
-tmux ls
-
-tmux send-keys -t portfolio.0 "source python3-virtualenv/bin/activate" ENTER
-
-tmux send-keys -t portfolio.0 "export FLASK_ENV=development" ENTER
-
-tmux send-keys -t portfolio.0 "flask run --host=0.0.0.0" ENTER
+systemctl start myportfolio
+systemctl enable myportfolio
