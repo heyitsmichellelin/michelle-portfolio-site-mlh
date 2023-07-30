4a4d7c55c51b6006ff76e4485a3ac59c35bd5726
iamwafula <kni8@mail.com>
2023-07-30 23:27:04 +0000

    Changed redeploy script

--- .redeploy-site.sh
+++ .redeploy-site.sh
@@ -1,5 +1,3 @@
 git fetch && git reset origin/main --hard
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,3 @@
-tmux kill-server
-source venv/bin/activate
 git fetch && git reset origin/main --hard
-pip install --upgrade pip
-pip install -r requirements.txt
-tmux new-session -d -s my_session \; send-keys "flask run --host=0.0.0.0" Enter
-
-
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
