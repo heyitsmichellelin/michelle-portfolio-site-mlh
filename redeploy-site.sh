a6d6f5514dba706cfb578a0c62c34d4348d77a72
“Michelle <itsmichellelin@gmail.com>
2023-08-02 20:59:34 -0700

    remove debug statements

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,12 +4,6 @@ cd portfolio-site-mlh/
 
 git fetch && git reset origin/main --hard
 
-echo "Pulled from GitHub"
-
 docker compose -f docker-compose.prod.yml down
 
-echo "just ran docker down"
-
 docker compose -f docker-compose.prod.yml up -d --build
-
-echo "just ran docker up"
