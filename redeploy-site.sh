09f8c02f3b3b04258772d07bf3d3947ec54b99f4
“Michelle <itsmichellelin@gmail.com>
2023-08-02 20:39:20 -0700

    echo statements to debug

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,6 +4,12 @@ cd portfolio-site-mlh/
 
 git fetch && git reset origin/main --hard
 
+echo "Pulled from GitHub"
+
 docker compose -f docker-compose.prod.yml down
 
+echo "just ran docker down"
+
 docker compose -f docker-compose.prod.yml up -d --build
+
+echo "just ran docker up"
