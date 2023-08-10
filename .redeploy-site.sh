588c1f5bc7529dbc8a0f33b88255fbe31aabf209
iamwafula <kni8@mail.com>
2023-08-10 20:23:03 +0000

    Cleaned repo dirt

--- .redeploy-site.sh
+++
@@ -1,3 +0,0 @@
-git fetch && git reset origin/main --hard
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
--- curl-test.sh
+++
@@ -1,9 +0,0 @@
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Ian&email=dummyEmail@gmail.com&content=Just Added to my database!'
-
-EMAIL=$(curl --request GET http://localhost:5000/api/timeline_post | jq -r '.timeline_posts | first | .email')
-
-if [ $EMAIL == "dummyEmail@gmail.com" ]; then
-    echo "Test Passed"
-else
-    echo "Test Failed"    
-fi
